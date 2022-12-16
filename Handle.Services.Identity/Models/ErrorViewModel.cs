096635f21d32eaa748f6be5c641981fbb974f738
jamesdoam <jamesdoam@gmail.com>
2022-12-16 23:45:51 +1100

    IdentityServer Add Application User and Configure API Scopes

---
+++ Handle.Services.Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Handle.Services.Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
